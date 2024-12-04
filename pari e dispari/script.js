22ee361a87184513239db1e316e571e80927ea08
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-04 23:44:21 +0100

    feat:add-exercise

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -3,3 +3,29 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+let userNUm= parseInt(prompt('scegli un numero da 1 a 5'))
+let userChoose= prompt(` 'Pari' o 'dispari'?` )
+let cpuNum= chooseCpuNum()
+
+function chooseCpuNum(){
+    return Math.floor(Math.random() * 5) + 1
+}
+
+let sum = userNUm + cpuNum
+
+let result;
+if(sum % 2 === 0){
+    result='pari'
+} else{
+    result= 'dispari'
+}
+
+console.log(cpuNum , sum, result)
+
+if((userChoose === 'pari' && result === 'pari') || (userChoose === 'dispari' && result === 'dispari')){
+    alert('Hai vinto!')
+} else{
+    alert('hai perso')
+}
+
